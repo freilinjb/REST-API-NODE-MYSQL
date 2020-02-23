2eb10e0f0ea5fce9de498a33382307febcf6d7e2
freilinjb <freilinjb@gmail.com>
2020-02-23 14:25:17 -0400

    agrergando nodemon

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+    
+});
